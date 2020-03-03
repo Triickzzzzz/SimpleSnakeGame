f51bcf2799027ffe91ee480dce5f126f35123a6e
Triickzzzzz <Kevin924mx.de>
2020-03-03 01:09:43 +0100

    fill app with objects; place snake and food

--- snake.py
+++ snake.py
@@ -6,8 +6,11 @@ class Snake(tk.Canvas):
     def __init__(self):
        super().__init__(width=600, height=600, background="black", highlightthickness=0)
        self.snake_positions = [(100,100), (80,100), (60,100)]
+       self.food_position = (200,200)
+       self.score = 0
 
        self.load_assets()
+       self.create_objects()
 
     def load_assets(self):
         try:
@@ -18,7 +21,16 @@ class Snake(tk.Canvas):
             self.food = ImageTk.PhotoImage(food_image)
         except IOError as error:
             print(error)
-            root.destroy()
+            root.destroy()  
+
+    def create_objects(self):
+        self.create_text(45, 12, text=f"Score: {self.score}", tag="score", fill="#fff", font=("TkDefaultFont",14))
+        self.create_rectangle(7, 27, 593, 593, outline="grey")
+
+        for x_pos, y_pos in self.snake_positions:
+            self.create_image(x_pos, y_pos, image=self.snake_body, tag="snake")
+        
+        self.create_image(self.food_position[0], self.food_position[1], image=self.food, tag="food")
 
 
 
@@ -26,8 +38,8 @@ root = tk.Tk()
 root.title("Snake")
 root.resizable(False, False)
 
-board = Snake()
-board.pack()
+canvas = Snake()
+canvas.pack()
 root.mainloop()
 
 
